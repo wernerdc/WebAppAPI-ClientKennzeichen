5301afd37d673a8356ec801f443ede7c38ec1524
wernerdc <manuel.werner3@gmx.de>
2024-11-22 11:05:32 +0100

    update1

--- WebAppAPI-ClientKennzeichen/MainWindow.xaml.cs
+++ WebAppAPI-ClientKennzeichen/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WebAppAPI_ClientKennzeichen.Models;
 
 namespace WebAppAPI_ClientKennzeichen
 {
@@ -16,18 +19,60 @@ namespace WebAppAPI_ClientKennzeichen
     /// </summary>
     public partial class MainWindow : Window
     {
+        private HttpClient client = new HttpClient();
+        private List<DataModel> _listInfos = new List<DataModel>();
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btnGetKennzeichen_Click(object sender, RoutedEventArgs e)
+        private async void btnGetKennzeichen_Click(object sender, RoutedEventArgs e)
         {
+            // https://localhost:7044/api/KennzeichenInfos
+            client.BaseAddress = new Uri("https://localhost:7077/api/KennzeichenInfos");
+            string strResult = await client.GetStringAsync(client.BaseAddress);
+            lBox.Items.Add(strResult);
+            DataModel[]? infos = await client.GetFromJsonAsync<DataModel[]>(client.BaseAddress);
+            if (infos == null)
+                return;
+            _listInfos = new List<DataModel>();
+            if (_listInfos == null)
+                return;
+
+            _listInfos.AddRange(infos);
+            dGridResult.ItemsSource = _listInfos;
+            //if (dGridResult.Columns.Contains("Id"))
+                //dGridResult.Columns["Id"].Visible = false;
 
         }
 
-        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        
+
+        private async void btnSetKennzeichen_Click(object sender, RoutedEventArgs e)
         {
+            // https://localhost:7044/api/KennzeichenInfos
+            client.BaseAddress = new Uri("https://localhost:7077/api/KennzeichenInfos");
+            
+            DataModel newData = _listInfos.Last();
+            //payload = JsonSerializer
+            //await client.PutAsJsonAsync<DataModel>(newData);
+            
+            //string strResult = await client.GetStringAsync(client.BaseAddress);
+            //lBox.Items.Add(strResult);
+            
+            DataModel[]? infos = await client.GetFromJsonAsync<DataModel[]>(client.BaseAddress);
+            if (infos == null)
+                return;
+            List<DataModel> listInfos = new List<DataModel>();
+            if (listInfos == null)
+                return;
+
+            listInfos.AddRange(infos);
+            dGridResult.ItemsSource = listInfos;
+            //if (dGridResult.Columns.Contains("Id"))
+            //dGridResult.Columns["Id"].Visible = false;
 
         }
     }
---
+++ WebAppAPI-ClientKennzeichen/Models/DataModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAppAPI_ClientKennzeichen.Models
+{
+    class DataModel
+    {
+    //    public Class1[] Property1 { get; set; }
+    //}
+
+    //public class Class1
+    //{
+        public int id { get; set; }
+        public string kennzeichen { get; set; }
+        public string ort { get; set; }
+        public string kreis { get; set; }
+        public string bundesland { get; set; }
+        public string wappenLink { get; set; }
+    }
+}
